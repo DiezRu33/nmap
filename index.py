dbff9bd10e429b078768d46369c144922e25a84d
@DiezRu33 <diegoruffa@hotmail.com>
2023-06-07 00:12:26 -0300

    modified

--- index.py
+++ index.py
@@ -2,3 +2,14 @@ import os
 
 nmap <host>
 sudo edtable -A FORWAR "196.174.0.105/1
+
+
+// ssh keygen generator
+ssh-keygen -t rsa -b 4096 "correo@correo.com"
+// verify if system running ssh key
+eval "${ssh-agent -s);
+// agrega ckave ssh al siguiente 
+ssh-add  ~/.ssh/id_rsa
+// key copy
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
